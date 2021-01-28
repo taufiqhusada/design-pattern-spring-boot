b0a30e384a7fef04c36d7b896ae201f59ff93ea1
Taufiq Husada Daryanto <13518058@std.stei.itb.ac.id>
2021-01-28 10:10:44 +0700

    init

---
+++ src/main/java/com/blibli/desingpattern/DesingpatternApplication.java
@@ -0,0 +1,13 @@
+package com.blibli.desingpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesingpatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesingpatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blibli/desingpattern/DesingpatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blibli.desingpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesingpatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
